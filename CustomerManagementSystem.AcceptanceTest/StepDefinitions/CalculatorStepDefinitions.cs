a48473c0ea62b3b72700f3e6937e80e08eacf405
AdjeteGuru <justinadjev@hotmail.com>
2024-11-23 21:05:28 +0000

    skeleton added

---
+++ CustomerManagementSystem.AcceptanceTest/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,40 @@
+namespace CustomerManagementSystem.AcceptanceTest.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        private int num1;
+        private int num2;
+        private int total;
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+           num1 = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            num2 = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            total = num1 + num2;
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            total += result;
+        }
+    }
+}
